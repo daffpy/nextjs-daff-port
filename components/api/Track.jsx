7df03798b1430343056b2f787803929a98cd7cd7
Hyde <codingdaffa@gmail.com>
2022-12-19 09:34:54 +0700

    Update Track.jsx

--- components/api/Track.jsx
+++ components/api/Track.jsx
@@ -7,7 +7,7 @@ export default function Track(track) {
         </p>
         <div className="flex flex-col pl-3">
           <a
-            className=" font-space text-[14px] font-bold text-gray-900 dark:text-gray-100 w-60 sm:w-96 md:w-full"
+            className=" font-space text-[14px] font-bold text-gray-900 dark:text-gray-100 w-80 sm:w-96 md:w-full"
             href={track.songUrl}
             target="_blank"
             rel="noopener noreferrer"
