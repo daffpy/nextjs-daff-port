bdae28775657bea7be8512da02890696035e69e0
Hyde <codingdaffa@gmail.com>
2022-12-21 21:34:01 +0700

    Update Profile.jsx

--- components/api/Profile.jsx
+++ components/api/Profile.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function Profile(data) {
 
     return (
-        <Image src={!data ? "https://api.lanyard.rest/820569882860716072.webp":`https://cdn.discordapp.com/avatars/820569882860716072/${data.url}.webp?size=512`} 
-        width={100}
-        height={100}
+        <Image src={'/profile.jpg'} 
+        width={130}
+        height={130}
         className='rounded-full'
         alt='urmom' />
     );
