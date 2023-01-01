a83a7918fb8af04e1838f256e415af93580bd1e2
Hyde <codingdaffa@gmail.com>
2023-01-01 07:18:06 +0700

    Update Profile.jsx

--- components/api/Profile.jsx
+++ components/api/Profile.jsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import profilePic from 'public/picture/profile7.jpg'
 
 
 export default function Profile(data) {
 
     return (
-        <Image src={'/picture/profile7.jpg'} 
+        <Image src={profilePic} 
         width={130}
         height={130}
         className='rounded-full'
-        alt='urmom' />
+        alt='urmom'
+        placeholder="blur" />
     );
   }
