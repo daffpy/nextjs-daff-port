768eed95ba1ed2541b704d56122f8530de657fa4
Hyde <codingdaffa@gmail.com>
2023-01-08 20:36:00 +0700

    Update About.jsx

--- components/page/About.jsx
+++ components/page/About.jsx
@@ -24,7 +24,7 @@ const AboutPage = () => {
         <div className="max-w-[550px] mx-auto mt-[160px]">
             <div className="mx-6">
                 <div className="mt-[110px] text-[44px] font-htiny text-left">
-                    <Image src={profilePic} classname="rounded-full" width={130} height={130} alt="pp" placeholder="blur" />
+                    <Image src={profilePic} style={{'borderRadius':'50%'}} width={130} height={130} alt="pp" placeholder="blur" />
                     <div className="mt-4  font-outfit font-bold tracking-wide">
                         About me
                     </div>   
