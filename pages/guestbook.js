2fb66a78bf23cef1d1f60df5956beff5fbadcc51
Hyde <codingdaffa@gmail.com>
2023-01-09 12:11:31 +0700

    Update guestbook.js

--- pages/guestbook.js
+++ pages/guestbook.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>About Me | Daffa - Developer & Pixel Artist</title>
+        <title>Guestbook | Daffa - Developer & Pixel Artist</title>
         <meta name="description" content="another person from Indonesia" />
         <link rel="icon" href="/webicon.png" />
       </Head>
