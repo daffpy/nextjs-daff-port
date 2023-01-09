9910df265fa67d52f7f29f1254c64d4ccdcc9aeb
Hyde <codingdaffa@gmail.com>
2023-01-09 12:08:41 +0700

    better discord auth (?)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.discordapp.com','i.scdn.co'],
-},
+  },
+  async redirects() {
+    return [
+      {
+        source: '/api/auth/signin',
+        destination: '/guestbook',
+        permanent: false,
+        has:[{
+          type: 'query',
+          key: 'error',
+          value: 'Callback'
+        }]
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+//import GithubProvider from "next-auth/providers/github"
 import DiscordProvider from 'next-auth/providers/discord'
 
 const scopes = ['identify'].join(' ')
@@ -7,10 +7,12 @@ const scopes = ['identify'].join(' ')
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    /*
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    */
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
@@ -34,6 +36,11 @@ export const authOptions = {
       }
       return token
     }
+  },
+  logger: {
+    error(code, ...message) {
+      console.log(code)
+    },
   }
   
 }
