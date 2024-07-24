df71d18efda22a7fdbabc26695811d8e844e0be9
daffpy <105541452+daffpy@users.noreply.github.com>
2024-07-24 20:24:17 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,22 +12,20 @@ const nextConfig = {
   images: {
     domains: ['cdn.discordapp.com','i.scdn.co'],
   },
-  /*
-  async redirects() {
+  async headers() {
     return [
       {
-        source: '/api/auth/signin',
-        destination: '/guestbook',
-        permanent: false,
-        has:[{
-          type: 'query',
-          key: 'error',
-          value: 'Callback'
-        }]
-      },
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ]
-  },
-  */
+  }
 }
 
 module.exports = nextConfig
